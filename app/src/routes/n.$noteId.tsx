c8c505e8113ee324c795e6e7280d216707b5533a
domody <domody11@gmail.com>
2025-12-02 06:15:35 +0000

    feat: basic note ui setup to store roadmap - not functional

--- app/src/routes/n.$noteId.tsx
+++ app/src/routes/n.$noteId.tsx
@@ -1,5 +1,26 @@
+import { SidebarLayout } from "@/components/layout/sidebar-layout";
+import { NoteSidebar } from "@/components/navigation/note-sidebar";
+import { Note } from "@/components/note/Note";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/n/$noteId")({
-  component: () => <div>Note page</div>,
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  const { noteId } = Route.useParams();
+
+  if (noteId == "new") {
+    return (
+      <SidebarLayout sidebar={<NoteSidebar />}>
+        <Note />
+      </SidebarLayout>
+    );
+  }
+
+  return (
+    <SidebarLayout sidebar={<NoteSidebar />}>
+      <Note />
+    </SidebarLayout>
+  );
+}
