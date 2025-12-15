d53ea29166a4e5c601bbb3c9eed6cd4e8af6a330
domody <domody11@gmail.com>
2025-12-15 19:24:39 +0000

    feat(memory): add missed file for LLMCallOptions object feature

--- syris_core/llm/provider.py
+++ syris_core/llm/provider.py
@@ -2,22 +2,29 @@ from ollama import chat, ChatResponse, AsyncClient
 
 from typing import Any, Dict, Literal
 
+from syris_core.types.llm import LLMCallOptions
 from syris_core.memory.working_memory import WorkingMemory
 from syris_core.util.logger import log
 
 class LLMProvider:
-    def __init__(self, working_memory: WorkingMemory, model_name: str = "gpt-oss", host="http://localhost:11434"):
+    def __init__(self, model_name: str = "gpt-oss"):
         self.model_name = model_name
-        self.working_memory = working_memory
         self.client = AsyncClient()
 
-    async def complete(self, system_prompt: str, format: Dict[str, Any] | Literal['', 'json'] | None = None, tools = None, think: Literal["low", "medium", "high"] = "low") -> ChatResponse:
-        messages = [
-                {"role": "system", "content": system_prompt},
-                *self.working_memory.get_context(),
-        ]
-        log("memory", f"[WorkingMemory] Previous Messages: {[*self.working_memory.get_context()]}")
+    async def complete(
+            self, 
+            call: LLMCallOptions
+        ) -> ChatResponse:
 
-        return await self.client.chat(model=self.model_name, messages=messages, format=format, think=think)
+        messages = [{"role": "system", "content": call.system_prompt}]
 
+        if call.memory:
+            messages.extend(call.memory)
+            log("memory", f"[WorkingMemory] Previous Messages: {call.memory}")
 
+        return await self.client.chat(
+            model=self.model_name,
+            messages=messages,
+            format=call.format,
+            think=call.think,
+        )
