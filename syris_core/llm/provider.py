130817c63920c5d17ae60ad696f28d70b20317c8
domody <domody11@gmail.com>
2025-12-15 17:24:02 +0000

    feat(provider): integrate AsyncClient for asynchronous chat handling

--- syris_core/llm/provider.py
+++ syris_core/llm/provider.py
@@ -1,4 +1,4 @@
-from ollama import chat, ChatResponse
+from ollama import chat, ChatResponse, AsyncClient
 
 from typing import Any, Dict, Literal
 
@@ -6,11 +6,11 @@ from syris_core.memory.working_memory import WorkingMemory
 from syris_core.util.logger import log
 
 class LLMProvider:
-    def __init__(self, working_memory: WorkingMemory, model_name: str = "gpt-oss"):
+    def __init__(self, working_memory: WorkingMemory, model_name: str = "gpt-oss", host="http://localhost:11434"):
         self.model_name = model_name
-
         self.working_memory = working_memory
-    
+        self.client = AsyncClient()
+
     async def complete(self, system_prompt: str, format: Dict[str, Any] | Literal['', 'json'] | None = None, tools = None, think: Literal["low", "medium", "high"] = "low") -> ChatResponse:
         messages = [
                 {"role": "system", "content": system_prompt},
@@ -18,8 +18,6 @@ class LLMProvider:
         ]
         log("memory", f"[WorkingMemory] Previous Messages: {[*self.working_memory.get_context()]}")
 
-        response: ChatResponse = chat(model=self.model_name, messages=messages, format=format, think=think)
-        log("llm", f"[Provider] Response generated as: {response}")
+        return await self.client.chat(model=self.model_name, messages=messages, format=format, think=think)
 
-        return response
 
