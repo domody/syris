20e5bebe76091e1a141849bef689d1cf684e0404
domody <domody11@gmail.com>
2026-01-27 04:41:23 +0000

    refactor(mobile): changed to monorepo structure and begin porting to mobile RN app

--- syris_nutrition/apps/web/components/nav/bottom-bar/wrapper.tsx
+++ syris_nutrition/apps/web/components/nav/bottom-bar/wrapper.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
 import { BottomBar } from "./bottom-bar";
+import { cn } from "@/lib/utils";
 
 export function BottomBarPageWrapper({
+  className,
   children,
 }: {
-  children: React.ReactNode;
+  className?: string;
+  children?: React.ReactNode;
 }) {
   return (
     <div className="relative w-screen h-screen">
-      <div className="h-[calc(100vh-4rem)] w-full overflow-x-hidden overflow-y-auto pb-2 no-scrollbar">
+      <div
+        className={cn(
+          "h-[calc(100vh-4rem)] w-full overflow-x-hidden overflow-y-auto pb-2 no-scrollbar",
+          className,
+        )}
+      >
         {children}
       </div>
       <BottomBar />
