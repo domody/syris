e1754972839be2daf4adbb88e48656a90f7ee073
domody <domody11@gmail.com>
2026-03-26 17:56:19 +0000

    feat: updated allowed origins for backend api

--- core/syris_core/src/syris_core/api/app.py
+++ core/syris_core/src/syris_core/api/app.py
@@ -7,10 +7,14 @@ from .routes.health import router as health_router
 from .routes.ingest import router as ingest_router
 from .routes.stream import router as stream_router
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# origins = [
+#     "http://localhost:3000",
+#     "http://localhost:3001",
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:3001",
+# ]
+
+origin_regex = r"^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$|^https:\/\/([a-zA-Z0-9-]+\.)*<domain>\.uk$"
 
 def create_app(settings: Settings) -> FastAPI:
     app = FastAPI(
@@ -24,7 +28,8 @@ def create_app(settings: Settings) -> FastAPI:
     app.include_router(stream_router)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        # allow_origins=origins,
+        allow_origin_regex=origin_regex,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
